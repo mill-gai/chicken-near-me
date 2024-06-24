50fa24f8e621a7ff84555afb39444763cc35ca6a
milltoy <pornratmill@gmail.com>
2024-06-24 16:13:05 -0400

    add close form button

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps';
+import { AnimationProp, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [GoogleMap],
+  imports: [GoogleMap, FontAwesomeModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent implements OnInit{
   openForm: boolean = false;
+  faCircleXmark = faCircleXmark;
+  animation:AnimationProp | undefined = undefined;
+
   options: google.maps.MapOptions = {
     center: {lat: 40, lng: 10},
     zoom: 2,
@@ -19,6 +24,12 @@ export class HomePageComponent implements OnInit{
   }
 
   addImageHandler() {
+    this.openForm = !this.openForm;
+    this.animation = undefined;
+  }
+
+  playIconAnimation() {
+    this.animation = "beat-fade";
     this.openForm = !this.openForm;
   }
 }
