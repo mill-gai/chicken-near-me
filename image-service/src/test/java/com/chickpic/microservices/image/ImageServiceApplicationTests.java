8ae3a1302dfcdda26dbeb1280b91e72aec8cb696
milltoy <pornratmill@gmail.com>
2024-06-28 15:36:00 -0400

    image service init

---
+++ image-service/src/test/java/com/chickpic/microservices/image/ImageServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.chickpic.microservices.image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
