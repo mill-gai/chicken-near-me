2ce0f447b8c8b7d0a143d54a79ebb03d241ea82f
milltoy <pornratmill@gmail.com>
2024-07-01 18:55:11 -0400

    map marker

---
+++ src/app/components/map-marker/map-marker.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { MapAdvancedMarker } from '@angular/google-maps';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Image } from '../../model/image';
+import { Position } from '../../model/position';
+import { ImageService } from '../../services/image/image.service';
+
+@Component({
+    selector: 'app-map-marker',
+    standalone: true,
+    imports: [MapAdvancedMarker, FontAwesomeModule],
+    templateUrl: './map-marker.component.html',
+    styleUrl: './map-marker.component.css',
+})
+export class MapMarkerComponent implements OnInit {
+    private readonly imageService = inject(ImageService);
+    @Input() country: string;
+    @Input() city: string;
+    @Input() images: Image[];
+    position: Position;
+    imgUrls: string[];
+    showContent: boolean = false;
+    faCircleXmark = faCircleXmark;
+
+    ngOnInit(): void {}
+
+    getImageUrl(fileNames: string[]) {
+        this.imageService.getImageUrls(fileNames).subscribe((response) => {
+            this.images.forEach(
+                (value, index) => (value.fileUrl = response[index])
+            );
+        });
+    }
+
+    onClickHandler() {
+        const fileNames: string[] = this.images.map((i) => i.fileName);
+        this.getImageUrl(fileNames);
+        console.log(this.images);
+        this.showContent = !this.showContent;
+    }
+}
