44474f46e98d223e149e21f1d7ce68c3e3eac9d4
milltoy <pornratmill@gmail.com>
2024-07-01 18:07:45 -0400

    add type

--- src/app/model/image.ts
+++ src/app/model/image.ts
@@ -6,3 +6,17 @@ export interface ImageInfo {
     fileName?: string;
     fileUrl?: string;
 }
+
+export type Image = {
+    title: string;
+    description: string;
+    fileName: string;
+};
+
+export type ImageByLocation = {
+    country: string;
+    cities: {
+        city: string;
+        images: Image[];
+    }[];
+};
