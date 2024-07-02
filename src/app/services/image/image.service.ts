af91b88be9d77952eefb1bc10c5d5f30237efbb5
milltoy <pornratmill@gmail.com>
2024-07-02 15:13:53 -0400

    format

--- src/app/services/image/image.service.ts
+++ src/app/services/image/image.service.ts
@@ -4,32 +4,42 @@ import { Observable } from 'rxjs';
 import { ImageInfo } from '../../model/image';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ImageService {
+    constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) {}
+    uploadImage(image: ImageInfo, file: File): Observable<string> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                Enctype: 'multipart/form-data',
+            }),
+            responseType: 'text' as 'json',
+        };
+        const formData: FormData = new FormData();
+        formData.append(
+            'imageRequest',
+            new Blob([JSON.stringify(image)], { type: 'application/json' })
+        );
+        formData.append('file', file);
+        // const body = { file: file, imageRequest: image};
+        return this.httpClient.post<string>(
+            'http://localhost:8080/api/image',
+            formData,
+            httpOptions
+        );
+    }
 
-  uploadImage(image: ImageInfo, file: File): Observable<string> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Enctype': 'multipart/form-data'
-      }),
-      responseType: 'text' as 'json'
-    };
-    const formData: FormData = new FormData();
-    formData.append('imageRequest', new Blob([JSON.stringify(image)], { type: 'application/json' }));
-    formData.append('file', file);
-    // const body = { file: file, imageRequest: image};
-    return this.httpClient.post<string>('http://localhost:8080/api/image', formData, httpOptions);
-  }
+    getAllImages(): Observable<ImageInfo[]> {
+        return this.httpClient.get<Array<ImageInfo>>(
+            'http://localhost:8080/api/image/getAllImages'
+        );
+    }
 
-  getAllImages() {
-    return this.httpClient.get<Array<ImageInfo>>('http://localhost:8080/api/image/getAllImages');
-  }
-  
-  getImageUrls(fileNames: string[]) {
-    return this.httpClient.get<Array<string>>('http://localhost:8080/api/image/getImageUrls', {params: {fileNames: fileNames}})
-  }
-
+    getImageUrls(fileNames: string[]): Observable<string[]> {
+        return this.httpClient.get<Array<string>>(
+            'http://localhost:8080/api/image/getImageUrls',
+            { params: { fileNames: fileNames } }
+        );
+    }
 }
