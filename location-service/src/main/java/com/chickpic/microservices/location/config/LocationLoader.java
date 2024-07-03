097a4e41e1784fa58440b5c3193ee9e6b6a47360
milltoy <pornratmill@gmail.com>
2024-07-03 13:05:51 -0400

    initialize db

---
+++ location-service/src/main/java/com/chickpic/microservices/location/config/LocationLoader.java
@@ -0,0 +1,32 @@
+package com.chickpic.microservices.location.config;
+
+import com.chickpic.microservices.location.model.Location;
+import com.chickpic.microservices.location.repository.LocationRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+public class LocationLoader implements CommandLineRunner {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<Location>> typeReference = new TypeReference<>(){};
+        InputStream inputStream = new ClassPathResource("locationData.json").getInputStream();
+        List<Location> locations = mapper.readValue(inputStream, typeReference);
+        System.out.println("locations: " + locations);
+        locationRepository.saveAll(locations);
+    }
+}
